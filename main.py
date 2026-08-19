ef475b8fac02a7b4e4d464af51406c79871bc799
Gopichand241975 <gopichand241975@gmail.com>
2026-08-19 18:55:34 +0530

    modified 4

--- main.py
+++ main.py
@@ -12,3 +12,9 @@ import argparse
 import time
 
 import cv2
+
+from detection import PersonDetector
+from tracker import PersonTracker
+from scoring_engine import compute_suspicion_score
+from alerts import send_alert, TamperMonitor
+from config import CAMERA_1_SOURCE, FORCED_ENTRY_MOTION_DWELL_SECONDS
